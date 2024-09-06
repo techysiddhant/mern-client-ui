095df02a5f6eb7ffebb77416661ab849a550347f
techysiddhant <sidjain8000@gmail.com>
2024-09-06 16:35:59 +0530

    add shadcn ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div className="">
       hello
+      <Button>Hello</Button>
     </div>
   );
 }
