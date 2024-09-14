87fb0bf49c18096f45d16e98eaa87eaaae28689c
techysiddhant <sidjain8000@gmail.com>
2024-09-14 17:34:51 +0530

    check cart item already exsist

--- src/app/(home)/components/product-modal.tsx
+++ src/app/(home)/components/product-modal.tsx
@@ -8,12 +8,14 @@ import Image from "next/image"
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog"
 import { Product, Topping } from "@/lib/types"
 import { startTransition, Suspense, useMemo, useState } from "react"
-import { useAppDispatch } from "@/lib/store/hooks";
-import { addToCart } from "@/lib/store/features/cart/cartSlice";
+import { useAppDispatch, useAppSelector } from "@/lib/store/hooks";
+import { addToCart, CartItem } from "@/lib/store/features/cart/cartSlice";
+import { hashTheItem } from "@/lib/utils";
 type ChosenConfig = {
     [key: string]: string
 }
 const ProductModal = ({ product }: { product: Product }) => {
+    const cartItems = useAppSelector((state) => state.cart.cartItems);
     const defaultConfiguration = Object.entries(product.category.priceConfiguration).map(([key, value]) => {
         return {
             [key]: value.availableOptions[0]
@@ -39,6 +41,22 @@ const ProductModal = ({ product }: { product: Product }) => {
         })
     }
     const [selectedToppings, setSelectedToppings] = useState<Topping[]>([]);
+    const alreadyHasInCart = useMemo(() => {
+        const currentConfiguration = {
+            _id: product._id,
+            name: product.name,
+            image: product.image,
+            priceConfiguration: product.priceConfiguration,
+            chosenConfiguration: {
+                priceConfiguration: { ...chosenConfig },
+                selectedToppings: selectedToppings
+            },
+            qty: 1
+        }
+        const hash = hashTheItem(currentConfiguration);
+        return cartItems.some((item: CartItem) => item.hash === hash)
+
+    }, [product, cartItems, chosenConfig, selectedToppings])
     const handleCheckBoxCheck = (topping: Topping) => {
         const isAlreadyExists = selectedToppings.some((selectedTopping: Topping) => selectedTopping.id === topping.id);
         startTransition(() => {
@@ -50,12 +68,16 @@ const ProductModal = ({ product }: { product: Product }) => {
         })
     }
     const handleAddToCart = (product: Product) => {
-        const itemToAdd = {
-            product,
+        const itemToAdd: CartItem = {
+            _id: product._id,
+            name: product.name,
+            image: product.image,
+            priceConfiguration: product.priceConfiguration,
             chosenConfiguration: {
                 priceConfiguration: chosenConfig!,
                 selectedToppings: selectedToppings
-            }
+            },
+            qty: 1
         }
         dispatch(addToCart(itemToAdd))
     }
@@ -69,6 +91,8 @@ const ProductModal = ({ product }: { product: Product }) => {
         }, 0);
         return configPricing + toppingsTotal
     }, [chosenConfig, selectedToppings, product.priceConfiguration])
+
+
     return (
         <Dialog>
             <DialogTrigger asChild >
@@ -121,9 +145,9 @@ const ProductModal = ({ product }: { product: Product }) => {
                                 ₹{totalPrice}
 
                             </span>
-                            <Button onClick={() => handleAddToCart(product)}>
+                            <Button className={alreadyHasInCart ? "bg-gray-800" : 'bg-primary'} disabled={alreadyHasInCart} onClick={() => handleAddToCart(product)}>
                                 <ShoppingCart size={20} />
-                                <span className="ml-2">Add to cart</span>
+                                <span className="ml-2">{alreadyHasInCart ? 'Already in cart' : 'Add to cart'}</span>
                             </Button>
                         </div>
                     </div>
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,12 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
-
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { CartItem } from "./store/features/cart/cartSlice";
+import CryptoJs from "crypto-js";
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+export function hashTheItem(payload: CartItem): string {
+  const jsonString = JSON.stringify({ ...payload, qty: undefined });
+  const hash = CryptoJs.SHA256(jsonString).toString();
+  return hash;
 }
