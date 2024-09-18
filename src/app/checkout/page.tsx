39a6c079bea87cc75b61888ffa2250a4187a1ccb
techysiddhant <sidjain8000@gmail.com>
2024-09-18 10:39:58 +0530

    add auth to checkout page

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import CustomerForm from './_components/CustomerForm';
+import { getSession } from '@/lib/session';
+import { redirect } from 'next/navigation';
 
-const CheckoutPage = ({
+const CheckoutPage = async ({
     searchParams,
 }: {
     searchParams: { restaurantId: string };
 }) => {
+    const session = await getSession();
+    if (!session) {
+        redirect('/login');
+    }
     const sParams = new URLSearchParams(searchParams);
     const existingQueryString = sParams.toString();
 
